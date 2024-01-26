68277de142571f89ca0191aea5442a2fa89b6253
KKKZOZ <kkkzoz@qq.com>
2024-01-26 10:00:47 +0800

    Format

--- pkg/txn/connector.go
+++ pkg/txn/connector.go
@@ -4,7 +4,8 @@ type Connector interface {
 	Connect() error
 	GetItem(key string) (DataItem, error)
 	PutItem(key string, value DataItem) (string, error)
-	ConditionalUpdate(key string, value DataItem, doCreate bool) (string, error)
+	ConditionalUpdate(key string,
+		value DataItem, doCreate bool) (string, error)
 	Get(name string) (string, error)
 	Put(name string, value any) error
 	Delete(name string) error
