9a947ddc5c554983dd395f539f82da73c31293b2
KKKZOZ <70520782+KKKZOZ@users.noreply.github.com>
2024-06-05 16:08:12 +0800

    Add debug option in native datastore

--- benchmarks/db/redis/db.go
+++ benchmarks/db/redis/db.go
@@ -4,7 +4,9 @@ import (
 	"benchmark/ycsb"
 	"context"
 	"sync"
+	"time"
 
+	"github.com/kkkzoz/oreo/pkg/config"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -51,21 +53,41 @@ func (r *Redis) CleanupThread(ctx context.Context) {
 }
 
 func (r *Redis) Read(ctx context.Context, table string, key string) (string, error) {
+
+	if config.Debug.DebugMode {
+		time.Sleep(config.Debug.ConnAdditionalLatency)
+	}
+
 	keyName := getKeyName(table, key)
 	return r.Rdb.Get(context.Background(), keyName).Result()
 }
 
 func (r *Redis) Update(ctx context.Context, table string, key string, value string) error {
+
+	if config.Debug.DebugMode {
+		time.Sleep(config.Debug.ConnAdditionalLatency)
+	}
+
 	keyName := getKeyName(table, key)
 	return r.Rdb.Set(context.Background(), keyName, value, 0).Err()
 }
 
 func (r *Redis) Insert(ctx context.Context, table string, key string, value string) error {
+
+	if config.Debug.DebugMode {
+		time.Sleep(config.Debug.ConnAdditionalLatency)
+	}
+
 	keyName := getKeyName(table, key)
 	return r.Rdb.Set(context.Background(), keyName, value, 0).Err()
 }
 
 func (r *Redis) Delete(ctx context.Context, table string, key string) error {
+
+	if config.Debug.DebugMode {
+		time.Sleep(config.Debug.ConnAdditionalLatency)
+	}
+
 	keyName := getKeyName(table, key)
 	return r.Rdb.Del(context.Background(), keyName).Err()
 }
